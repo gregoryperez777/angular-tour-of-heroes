ab70b036b0f7c94e2458a9126b42a11082ccda92
Gregory Perez <gregoryperezfigueredo@gmail.com>
2019-01-15 10:55:17 -0400

    lesson seven 7.3

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { MessageService } from './message.service';
 import { Observable, of } from 'rxjs';
 import { Hero } from './hero';
@@ -7,19 +8,26 @@ import { HEROES } from './mock-heroes';
 @Injectable({
   providedIn: 'root'
 })
+
 export class HeroService {
+  constructor(
+    private http: HttpClient, 
+    private messageService: MessageService) { }
 
-  constructor(private messageService: MessageService) { }
-
-  getHeroes(): Observable<Hero[]> {
-    // TODO: send the message _after_ fetching the heroes
-    this.messageService.add('HeroService: fetched heroes');
-    return of(HEROES);
-  }
+  private heroesUrl = 'api/heroes'; 
 
   getHero(id: number): Observable<Hero> {
     // TODO: send the message _after_ fetching the hero
     this.messageService.add(`HeroService: fetched hero id=${id}`);
     return of(HEROES.find(hero => hero.id === id));
+  }
+
+  /** Log a HeroService message with the MessageService */
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
+  }
+
+  getHeroes (): Observable<Hero[]> {
+    return this.http.get<Hero[]>(this.heroesUrl)
   }
 }
